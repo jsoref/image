f432cfd39ffbb22584094f0033b41e42eb415697
Josh Soref <jsoref@users.noreply.github.com>
2020-12-24 11:36:25 -0500

    spelling: marshaling
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- docker/docker_client_test.go
+++ docker/docker_client_test.go
@@ -110,7 +110,7 @@ func TestNewBearerTokenFromInvalidJsonBlob(t *testing.T) {
 	tokenBlob := []byte("IAmNotJson")
 	_, err := newBearerTokenFromJSONBlob(tokenBlob)
 	if err == nil {
-		t.Fatalf("unexpected an error unmarshalling JSON")
+		t.Fatalf("unexpected an error unmarshaling JSON")
 	}
 }
 
