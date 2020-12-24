59ba29bf6140be1183d62aeb5067c1c07ca8a661
Josh Soref <jsoref@users.noreply.github.com>
2020-12-24 10:49:31 -0500

    spelling: containers
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- pkg/sysregistriesv2/system_registries_v2.go
+++ pkg/sysregistriesv2/system_registries_v2.go
@@ -450,7 +450,7 @@ func newConfigWrapperWithHomeDir(ctx *types.SystemContext, homeDir string) confi
 	return wrapper
 }
 
-// ConfigurationSourceDescription returns a string containres paths of registries.conf and registries.conf.d
+// ConfigurationSourceDescription returns a string containers paths of registries.conf and registries.conf.d
 func ConfigurationSourceDescription(ctx *types.SystemContext) string {
 	wrapper := newConfigWrapper(ctx)
 	configSources := []string{wrapper.configPath}
